6b21f470703347a8296185454f30eec1f3d2a581
刘权 <1452764349@qq.com>
2021-02-02 00:50:29 +0800

    这是我的第一个JAVA项目

---
+++ MyProjects/src/com/quan/MyTest.java
@@ -0,0 +1,12 @@
+package com.quan;
+
+import java.sql.SQLOutput;
+
+/**
+ * Created by DELL on 2021/2/2
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("这是我的第一个JAVA项目");
+    }
+}
